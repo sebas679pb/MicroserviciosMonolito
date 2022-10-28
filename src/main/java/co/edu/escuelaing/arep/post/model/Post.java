9c43e0f0433f6fcfae7b014b88e0a1e4a3287559
unknown <jhon.pineros@labinfo.is.escuelaing.edu.co>
2022-10-28 12:56:25 -0500

    first commit

---
+++ src/main/java/co/edu/escuelaing/arep/post/model/Post.java
@@ -0,0 +1,19 @@
+package co.edu.escuelaing.arep.post.model;
+
+public class Post {
+
+    private String text;
+
+    public Post(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+}
